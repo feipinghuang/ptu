3fb44008b65c189ed2c8c3bfdf7edf2f16fdafb9
feipinghuang <feiping.huang@thexstore.cn>
2018-03-20 18:08:18 +0800

    add image preview

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: redirect("/templates")
   resources :templates
   resources :actors, only: [:create, :show]
   resources :mergings, only: [:create, :show]
