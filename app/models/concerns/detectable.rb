12a5fc79f4a2f3ec7018ac8301df295306331b21
feipinghuang <feiping.huang@thexstore.cn>
2018-03-20 16:02:21 +0800

    add deploy

--- app/models/concerns/detectable.rb
+++ app/models/concerns/detectable.rb
@@ -5,12 +5,6 @@ module Detectable
     has_one_attached :image
   end
 
-  def detect_rectangle
-    return if image.blank?
-    rectangle = Facepp.new.detect_face_rectangle(image_file)
-    self.update(rectangle: rectangle)
-  end
-
   def image_file
     Tempfile.new([image.filename.base, image.filename.extension_with_delimiter]).tap do |file|
       file.binmode
